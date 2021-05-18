3b8abe30081904a7c09b22d6c5006410a9d1d50c
Mark <marktim39@gmail.com>
2021-05-18 14:26:58 +0600

    14_lpr

---
+++ 14/calc.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Калькулятор</title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+  <div id="header">
+    <img src="logo.gif" width="187" height="29" alt="Наш логотип" class="logo" />
+    <span class="slogan">приходите к нам учиться</span>
+  </div>
+  
+  <div id="content">
+    <h1>Калькулятор школьника</h1>
+    <form action=''>
+      <label>Число 1:</label>
+      <br />
+      <input name='num1' type='text' />
+      <br />
+      <label>Оператор: </label>
+      <br />
+      <input name='operator' type='text' />
+      <br />
+      <label>Число 2: </label>
+      <br />
+      <input name='num2' type='text' />
+      <br />
+      <br />
+      <input type='submit' value='Считать'>
+    </form>
+  </div>
+
+  <div id="nav">
+    <h2>Навигация по сайту</h2>
+    <ul>
+      <li><a href='index.php'>Домой</a>
+      </li>
+      <li><a href='about.php'>О нас</a>
+      </li>
+      <li><a href='contact.php'>Контакты</a>
+      </li>
+      <li><a href='table.php'>Таблица умножения</a>
+      </li>
+      <li><a href='calc.php'>Калькулятор</a>
+      </li>
+    </ul>
+  </div>
+
+  <div id="footer">
+    &copy; Супер Мега Веб-мастер, 2000 &ndash; 2021
+  </div>
+</body>
+</html>
