85d5d8bd81496ce31591b3b364fb37a7d47522c8
Mark <marktim39@gmail.com>
2021-05-18 14:27:47 +0600

    16_lpr

---
+++ 16/index.php
@@ -0,0 +1,71 @@
+<?php
+  include "inc/lib.inc.php";
+  include "inc/data.inc.php";
+  $title = 'Сайт нашей школы';
+  $header = "$welcome, Гость!";
+  $id = strtolower(strip_tags(trim($_GET['id'])));
+  switch($id){
+  case 'about':
+  $title = 'О сайте';
+  $header = 'О нашем сайте';
+  break;
+  case 'contact':
+  $title = 'Контакты';
+  $header = 'Обратная связь';
+  break;
+  case 'table':
+  $title = 'Таблица умножения';
+  $header = 'Таблица умножения';
+  break;
+  case 'calculator':
+  $title = 'Он-лайн калькулятор';
+  $header = 'Калькулятор';
+  break;
+  }
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+  <title><?php echo $title?></title>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+</head>
+
+<body>
+
+  <div id="header">
+    <?php include "inc/top.inc.php" ?>
+  </div>
+
+  <div id="content">
+    <h1><?php echo $header?></h1>
+    <?php
+      switch($id){
+      case 'about':
+      include 'about.php';
+      break;
+      case 'contact':
+      include 'contact.php';
+      break;
+      case 'table':
+      include 'table.php';
+      break;
+      case 'calculator':
+      include 'calc.php';
+      break;
+      default:
+      include 'inc/index.inc.php';
+      }
+    ?>
+  </div>
+  
+  <div id="nav">
+    <?php include "inc/menu.inc.php" ?>
+  </div>
+
+  <div id="footer">
+    <?php include "inc/bottom.inc.php" ?>
+  </div>
+</body>
+</html>
---
+++ 16/table.php
@@ -0,0 +1,29 @@
+<?php
+if($_SERVER['REQUEST_METHOD'] == 'POST'){
+$cols = abs((int) $_POST['cols']);
+$rows = abs((int) $_POST['rows']);
+$color = trim(strip_tags($_POST['color']));
+}
+$cols = ($cols) ? $cols : 10;
+$rows = ($rows) ? $rows : 10;
+$color = ($color) ? $color : 'red';
+?>
+    <form action='<?= $_SERVER['REQUEST_URI']?>' method="POST">
+      <label>Количество колонок: </label>
+      <br />
+      <input name='cols' type='text' value="<?=$cols?>" />
+      <br />
+      <label>Количество строк: </label>
+      <br />
+      <input name='rows' type='text' value="<?=$rows?>" />
+      <br />
+      <label>Цвет: </label>
+      <br />
+      <input name='color' type='text' value="<?=$color?>" />
+      <br />
+      <br />
+      <input type='submit' value='Создать' />
+    </form>
+    <?php
+    drawTable($cols, $rows, $color);
+    ?>
